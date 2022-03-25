cafa987e390f9d31af134b7c9965f143a49ad7de
Alex <alexandrestefanoss@gmail.com>
2022-03-25 11:46:55 +0100

    25/03/22

--- puissance4.py
+++ puissance4.py
@@ -1,4 +1,4 @@
-matrix = [[0, 0, 0, 0, 0, 0, 0], 
+matrix=[[0, 0, 0, 0, 0, 0, 0], 
         [0, 0, 0, 0, 0, 0, 0], 
         [0, 0, 0, 0, 0, 0, 0], 
         [0, 0, 0, 0, 0, 0, 0], 
@@ -15,16 +15,17 @@ matrix = [[0, 0, 0, 0, 0, 0, 0],
 
 #               EN JEU
 #           1  2  3  4  5  6  7
-#    6    [[1, 0, 0, 0, 0, 0, 0], 
-#    5    [0, 2, 0, 0, 0, 0, 0], 
-#    4    [0, 0, 1, 0, 0, 0, 0], 
-#    3    [0, 0, 0, 1, 0, 0, 0], 
-#    2    [0, 0, 0, 0, 1, 0, 0], 
-#    1    [0, 0, 0, 0, 0, 1, 0]]
+#    6    [[0, 0, 0, 0, 0, 0, 0], 
+#    5    [0, 0, 0, 0, 0, 0, 0], 
+#    4    [0, 0, 0, 0, 0, 0, 0], 
+#    3    [0, 0, 0, 0, 0, 0, 0], 
+#    2    [0, 0, 0, 0, 0, 0, 0], 
+#    1    [0, 0, 0, 0, 0, 0, 0]]
 ligne = 0
 colonne = 0
 conditionFinPartie = False
 conditionRejouer = True
+print("Salut à tous")
 
 def affichage(matrix):
     for i in range(6):
@@ -37,30 +38,39 @@ def victoire(matrix, colonne, ligne):
     compteurJ1 = 0
     compteurJ2 = 0
     i = 0
+    tmpLigne = 0
+    tmpColonne = 0
+    while(tmpLigne < 6):
+        while(tmpColonne < 7):
+            if(matrix[tmpLigne][tmpColonne] == 1):
+                compteurJ1 += 1
+            else:
+                compteurJ1 = 0
+            if(matrix[tmpLigne][tmpColonne] == 2):
+                compteurJ2 += 1
+            else:
+                compteurJ2 = 0
+            if(compteurJ1 >= 4 or compteurJ2 >= 4):
+                return(True)
+            tmpColonne += 1
+        tmpLigne += 1
+        tmpColonne = 0
+    tmpLigne = 0
+    tmpColonne = 0
+    while(tmpLigne < 6):
+        if(matrix[tmpLigne][colonne] == 1):
+            compteurJ1 += 1
+        else:
+            compteurJ1 = 0
+        if(matrix[tmpLigne][colonne] == 2):
+            compteurJ2 += 1
+        else:
+            compteurJ2 = 0
+        if(compteurJ1 >= 4 or compteurJ2 >= 4):
+            return(True)
+        tmpLigne += 1
     tmpLigne = ligne
     tmpColonne = colonne
-    for i in range(6):
-        if(matrix[ligne][i] == 1):
-            compteurJ1 += 1
-        else:
-            compteurJ1 = 0
-        if(matrix[ligne][i] == 2):
-            compteurJ2 += 1
-        else:
-            compteurJ2 = 0
-        if(compteurJ1 >= 4 or compteurJ2 >= 4):
-            return(True)
-    for i in range(6):
-        if(matrix[i][colonne] == 1):
-            compteurJ1 += 1
-        else:
-            compteurJ1 = 0
-        if(matrix[i][colonne] == 2):
-            compteurJ2 += 1
-        else:
-            compteurJ2 = 0
-        if(compteurJ1 >= 4 or compteurJ2 >= 4):
-            return(True)
     while(tmpLigne > 0 and tmpColonne > 0):
         tmpLigne -= 1
         tmpColonne -= 1
@@ -85,9 +95,6 @@ def victoire(matrix, colonne, ligne):
     while(tmpLigne < 6 and tmpColonne >= 0):
         if(matrix[tmpLigne][tmpColonne] == 1):
             compteurJ1 += 1
-            print("compteurJ1 :", compteurJ1)
-            print("tmpLigne :", tmpLigne)
-            print("tmpColonne :", tmpColonne)
         else:
             compteurJ1 = 0
         if(matrix[tmpLigne][tmpColonne] == 2):
@@ -99,49 +106,104 @@ def victoire(matrix, colonne, ligne):
         tmpLigne += 1
         tmpColonne -= 1
 
-while(conditionRejouer == True): #ou que la grille soit pleine
-    print("Salut à tous")
+def trouverLigne(matrix, colonne):
+    ligne = 0
+    while(matrix[ligne][colonne] != 0):
+        ligne += 1
+    return(ligne)
+
+def jouerTour(matrix, joueur):
+    error = True
+    if(joueur == 1):    
+        print("Joueur 1, dans quelle case voulez-vous mettre votre jeton ?")
+        while(error == True):
+            error = False
+            colonne = int(input("Veuillez entrer le numéro de colonne\n"))
+            colonne -= 1
+            ligne = trouverLigne(matrix, colonne)
+            if(matrix[ligne][colonne] == 1 or matrix[ligne][colonne] == 2):
+                error = True
+                print("Il y a déjà un jeton dans cette case, veuillez rejouer votre tour")
+        matrix[ligne][colonne] = 1
+    else:
+        print("Joueur 2, dans quelle case voulez-vous mettre votre jeton ?")
+        while(error == True):
+            error = False
+            colonne = int(input("Veuillez entrer le numéro de colonne\n"))
+            colonne -= 1
+            ligne = trouverLigne(matrix, colonne)
+            if(matrix[ligne][colonne] == 1 or matrix[ligne][colonne] == 2):
+                error = True
+                print("Il y a déjà un jeton dans cette case, veuillez rejouer votre tour")
+        matrix[ligne][colonne] = 2
+
+def choixModeDeJeu():
     print("Explication des règles")
     print("Choisir un mode de jeu parmis :")
     print("- Joueur contre Joueur (tapez 0)")
     print("- Joueur contre IA (tapez 1)")
-    tmp = int(input(""))
-    while(tmp != 0 and tmp != 1):
-        if(tmp == 0):
+    mode = int(input(""))
+    while(mode != 0 and mode != 1):
+        if(mode == 0):
             print("Vous avez choisi le mode Joueur contre Joueur")
-        elif(tmp == 1):
+        elif(mode == 1):
             print("Vous avez choisi le mode Joueur contre IA")
         else:
             print("Veuillez choisir entre les 2 modes de jeux disponibles")
-    #bien faire l'affichage
-    if(tmp == 0):
+    return(mode)
+
+def rejouer():
+    rejouer = 9
+    while(rejouer != 0 and rejouer != 1):
+        rejouer = int(input("Voulez-vous relancer une partie ? 1 si oui, 0 sinon\n"))
+    if(rejouer == 1):
+        return(True)
+    else:
+        return(False)
+
+def testMatrixPleine(matrix):
+    tmpLigne = 0
+    caseVide = False
+    while(tmpLigne < 6):
+        tmpColonne = 0
+        while(tmpColonne < 7):
+            if(matrix[tmpLigne][tmpColonne] == 0):
+                caseVide = True
+            tmpColonne += 1
+        tmpLigne += 1
+    if(caseVide == False):
+        print("Il n'y a plus de cases disponibles")
+    return(caseVide)
+
+def viderMatrix():
+    matrix = [[0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0]]
+    return(matrix)
+
+while(conditionRejouer == True): #ou que la grille soit pleine
+    mode = choixModeDeJeu()
+    if(mode == 0):
         while(conditionFinPartie == False):
-            print("Joueur 1, dans quelle case voulez-vous mettre votre jeton ? [ligne, colonne]")
-            ligne = int(input("Veuillez entrer le numéro de ligne\n"))
-            ligne -= 1
-            colonne = int(input("Veuillez entrer le numéro de colonne\n"))
-            colonne -= 1
-            matrix[ligne][colonne] = 1
+            jouerTour(matrix, 1)
             affichage(matrix)
-            if(victoire(matrix, colonne, ligne) == True):
+            if(victoire(matrix, colonne, ligne) == True or testMatrixPleine(matrix) == False):
                 conditionFinPartie = True
                 print("GGGGGGGGGGGGGGGGG")
-            print("Joueur 2 ?")
-            ligne = int(input("Veuillez entrer le numéro de ligne\n"))
-            ligne -= 1
-            colonne = int(input("Veuillez entrer le numéro de colonne\n"))
-            colonne -= 1
-            matrix[ligne][colonne] = 2
-            affichage(matrix)
-            if(victoire(matrix, colonne, ligne) == True):
-                conditionFinPartie = True
-                print("GGGGGGGGGGGGGGGGGGGGG")
+            if(conditionFinPartie == False):
+                jouerTour(matrix, 2)
+                affichage(matrix)
+                if(victoire(matrix, colonne, ligne) == True or testMatrixPleine(matrix) == False):
+                    conditionFinPartie = True
+                    print("GGGGGGGGGGGGGGGGGGGGG")
     else:
         while(conditionFinPartie == False):
-            print("Joueur 1, dans quelle case voulez-vous mettre votre jeton ?")
-    conditionRejouer = False
-
-    # à faire :
-    #     - pour l'utilisateur : il ne doit mettre que les colonnes pour jouer et donc faire coulisser les pièces
-    #     - ne pas pouvoir remplacer une pièce déjà mise
-    #     - quand un des 2 joueurs gagnent une partie, la partie s'arrête
+            print("")
+            #implémenter joueur vs IA
+    conditionRejouer = rejouer()
+    if(conditionRejouer == True):
+        matrix = viderMatrix()
+        conditionFinPartie = False
